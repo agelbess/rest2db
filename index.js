90e48ecafc0fcd3be84b04e7eba6e9262529efdf
Alexandros Gelbessis <alexandros.gelmpesis@vodafone.com>
2019-12-16 10:39:22 +0200

    added delay

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const curl = require('./curl')
 const mssql = require('./mssql')
 
+var done = 0
 async function start() {
     const ids = await mssql.getIds()
-    let done = 0;
     ids.forEach(async (element, i) => {
-        let data = await curl.getData(element.id) // status and location
-        let response = await mssql.updateContainer(data.cntrNo, data.statusNm, data.placeNm, data.error)
-        console.log(`saved id: ${data.cntrNo}, status: ${data.statusNm}, location: ${data.placeNm}, error: ${data.error}`)
-        done++
-        if (done == ids.length) {
-            process.exit()
-        }
+        setTimeout(() => doOnContainer(element, ids), i * 100)
     });
+}
+
+async function doOnContainer(element, ids) {
+    let data = await curl.getData(element.id) // status and location
+    let response = await mssql.updateContainer(data.cntrNo, data.statusNm, data.placeNm, data.error)
+    console.log(`saved id: ${data.cntrNo}, status: ${data.statusNm}, location: ${data.placeNm}, error: ${data.error}`)
+    done++
+    if (done == ids.length) {
+        process.exit()
+    }
 }
 
 start();
